2b9dcd7b26f5f7527b488c8d619f5dd0dfc29dfb
Kryo <74908517+KryoBit@users.noreply.github.com>
2025-05-16 05:29:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+const displayTime = document.querySelector(".display-time");
+
+function showTime() {
+  let time = new Date();
+  displayTime.innerText = time.toLocaleTimeString("en-US", { hour12: true, hour: '2-digit', minute: '2-digit' });
+  setTimeout(showTime, 1000);
+}
+
+showTime();
+
+function updateDate() {
+  let today = new Date();
+
+  let dayName = today.getDay(),
+    dayNum = today.getDate(),
+    month = today.getMonth(),
+    year = today.getFullYear();
+
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const dayWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const IDCollection = ["day", "daynum", "month", "year"];
+  const val = [dayWeek[dayName], dayNum, months[month], year];
+  for (let i = 0; i < IDCollection.length; i++) {
+    document.getElementById(IDCollection[i]).firstChild.nodeValue = val[i];
+  }
+}
+
+updateDate();
+
+function toggleFullscreen() {
+  if (!document.fullscreenElement) {
+    document.body.requestFullscreen();
+  } else {
+    document.exitFullscreen();
+  }
+}
